9b46eddc5c78928428fb0152f483ab902e253c22
George Tolakis <tolakis@gmail.com>
2025-11-08 01:09:34 +0200

    commit

--- app/state.py
+++ app/state.py
@@ -21,6 +21,7 @@ from .indexing import IncrementalIndexManager, load_cached_nodes_and_index
 from .logger import LOGGER
 from .sessions import SessionManager
 from .session_uploads import SessionUploadManager, SessionUploadChunk
+assert SessionUploadManager is not None
 
 
 
